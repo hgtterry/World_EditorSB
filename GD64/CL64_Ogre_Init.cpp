0107df574f7a61f938b01522d997c3f8ac33b3c1
hgtterry <terryinflanite@gmail.com>
2024-05-30 21:12:21 +0100

    30/05/24 GD64 Setup Ogre Stuff

--- GD64/CL64_Ogre_Init.cpp
+++ GD64/CL64_Ogre_Init.cpp
@@ -1,3 +1,26 @@
+/*
+Copyright (c) GameDirector 2019 To 2024 HGT Software W.T.Flanigan H.C.Flanigan
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+claim that you wrote the original software. If you use this software
+in a product, an acknowledgment in the product documentation would be
+appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and must not be
+misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source
+distribution.
+*/
+
 #include "pch.h"
 #include "CL64_App.h"
 #include "CL64_Ogre_Init.h"
@@ -6,6 +29,7 @@ CL64_Ogre_Init::CL64_Ogre_Init(void)
 {
 	mRoot = nullptr;
 
+	mResourcePath = "";
 	App_Resource_Group = "App_Resource_Group";
 }
 
@@ -20,6 +44,8 @@ void CL64_Ogre_Init::InitOgre(void)
 {
 	OgreCreateRoot();
 	SetUpResources();
+
+	App->Say("Ogre Init Done");
 }
 
 // *************************************************************************
@@ -40,7 +66,6 @@ bool CL64_Ogre_Init::OgreCreateRoot(void)
 		mRoot = OGRE_NEW Ogre::Root(pluginsPath, mResourcePath + "Equity_CFG.cfg", mResourcePath + "");
 	}*/
 
-	App->Say("Ogre Init");
 	return 1;
 }
 
